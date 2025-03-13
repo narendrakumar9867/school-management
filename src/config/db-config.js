aacfda43e7d0ab241f997597364d4062d9901b91
mrnarendra9867 <narendrakumark750@gmail.com>
2025-03-14 04:53:25 +0530

    .env

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -1,8 +1,13 @@
 const { Sequelize } = require('sequelize');
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const sequelize = new Sequelize('school_management', 'root', 'Sita123@@', {
-    host: '127.0.0.1',
-    dialect: 'mysql'
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: 3306,
+    logging: false,
 });
 
 module.exports = sequelize;
